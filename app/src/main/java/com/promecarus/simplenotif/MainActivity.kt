b6bf598205625fa8b726b125360a22e26034e94f
promecarus <haikalslipi@gmail.com>
2023-09-18 13:06:57 +0700

    finish

--- app/src/main/java/com/promecarus/simplenotif/MainActivity.kt
+++ app/src/main/java/com/promecarus/simplenotif/MainActivity.kt
@@ -5,13 +5,13 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
+import androidx.core.app.TaskStackBuilder
 import com.promecarus.simplenotif.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -41,20 +41,38 @@ class MainActivity : AppCompatActivity() {
         binding.btnSendNotification.setOnClickListener {
             sendNotification(title, message)
         }
+
+        binding.btnOpenDetail.setOnClickListener {
+            startActivity(Intent(this@MainActivity, DetailActivity::class.java).apply {
+                putExtra(DetailActivity.EXTRA_TITLE, title)
+                putExtra(DetailActivity.EXTRA_MESSAGE, message)
+            })
+        }
     }
 
     private fun sendNotification(title: String, message: String) {
-        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://dicoding.com"))
-        val pendingIntent = PendingIntent.getActivity(
-            this, 0, intent, PendingIntent.FLAG_IMMUTABLE
-        )
+        val notifDetailIntent = Intent(this, DetailActivity::class.java)
+        notifDetailIntent.putExtra(DetailActivity.EXTRA_TITLE, title)
+        notifDetailIntent.putExtra(DetailActivity.EXTRA_MESSAGE, message)
+        val pendingIntent = TaskStackBuilder.create(this).run {
+            addNextIntentWithParentStack(notifDetailIntent)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                getPendingIntent(
+                    NOTIFICATION_ID,
+                    PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+                )
+            } else {
+                getPendingIntent(NOTIFICATION_ID, PendingIntent.FLAG_UPDATE_CURRENT)
+            }
+        }
 
         val notificationManager =
             getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        val builder = NotificationCompat.Builder(this, CHANNEL_ID).setContentIntent(pendingIntent)
-            .setContentTitle(title).setSmallIcon(R.drawable.baseline_notifications_active_24)
-            .setContentText(message).setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .setSubText(getString(R.string.notification_subtext))
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID).setContentTitle(title)
+            .setSmallIcon(R.drawable.baseline_notifications_active_24).setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setSubText(getString(R.string.notification_subtext)).setContentIntent(pendingIntent)
+            .setAutoCancel(true)
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(
